80eb891472c009be79baaf4d63caee2a4edca063
unknown <oluwadareseyii@gmail.com>
2020-09-22 09:42:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Home from "./pages/Home";
 import "./styles/app.scss";
 
 function App() {
-  return <div className="pages"></div>;
+  return (
+    <>
+      <div className="pages">
+        <Home />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="landing">
+      <section className="hero">Hi, this is home.</section>
+    </div>
+  );
+};
+
+export default Home;
